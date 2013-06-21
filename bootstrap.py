51be93ea3670f67288d4b575e822033138ea0644
Zhang Erning <zhang@erning.com>
2013-06-21 16:22:13 +0800

    upgrade to ve-v1.1

--- bootstrap.py
+++ bootstrap.py
@@ -25,9 +25,9 @@ class Bootstrap(object):
         if not os.path.isdir(self.bootstrap_dir):
             os.makedirs(self.bootstrap_dir)
 
-        logger.debug("working_dir: %s", self.working_dir)
-        logger.debug("bootstrap_dir: %s", self.bootstrap_dir)
-        logger.debug("virtualenv_dir: %s", self.virtualenv_dir)
+        logger.debug('working_dir: %s', self.working_dir)
+        logger.debug('bootstrap_dir: %s', self.bootstrap_dir)
+        logger.debug('virtualenv_dir: %s', self.virtualenv_dir)
 
         self.install_virtualenv()
         self.install_ve()
@@ -36,17 +36,17 @@ class Bootstrap(object):
     def _config_dot_dir(self, env_name, default_value=None):
         value = os.environ.get(env_name)
         if not value:
-            return "%s/%s" % (self.working_dir, default_value)
+            return '%s/%s' % (self.working_dir, default_value)
 
-        logger.debug("config %s from envron", env_name)
+        logger.debug('config %s from envron', env_name)
         if os.path.isabs(value):
             return value
         else:
-            return "%s/%s" % (self.working_dir, value)
+            return '%s/%s' % (self.working_dir, value)
 
 
     def install_virtualenv(self):
-        if os.path.isfile("%s/%s" % (self.virtualenv_dir, 'bin/activate')):
+        if os.path.isfile('%s/%s' % (self.virtualenv_dir, 'bin/activate')):
             return
 
         executable = None
@@ -57,14 +57,14 @@ class Bootstrap(object):
             pass
 
         if not executable:
-            executable = "%s/%s" % (self.bootstrap_dir, "virtualenv")
-            self.download("https://raw.github.com/pypa/virtualenv/1.9.1/virtualenv.py",
-                          executable, "3c152e15898578db937d5253d1478b39")
+            executable = '%s/%s' % (self.bootstrap_dir, 'virtualenv')
+            self.download('https://raw.github.com/pypa/virtualenv/1.9.1/virtualenv.py',
+                          executable, '3c152e15898578db937d5253d1478b39')
             self.mark_executable(executable)
 
         os.chdir(self.bootstrap_dir)
         try:
-            os.system("%s --distribute --no-site-packages '%s'" % (executable, self.virtualenv_dir))
+            os.system('%s --distribute --no-site-packages "%s"' % (executable, self.virtualenv_dir))
         finally:
             os.chdir(self.working_dir)
 
@@ -72,12 +72,12 @@ class Bootstrap(object):
     def download(self, source, target, hashing=None):
         if hashing and os.path.isfile(target) and hashing == self.md5sum(target):
             return
-        if os.system("wget '%s' -O '%s'" % (source, target)) == 0:
+        if os.system('wget "%s" -O "%s"' % (source, target)) == 0:
             return
-        if os.system("curl '%s' -o '%s'" % (source, target)) == 0:
+        if os.system('curl "%s" -o "%s"' % (source, target)) == 0:
             return
 
-        logger.error("Unable to download '%s'" % source)
+        logger.error('Unable to download "%s"' % source)
         raise RuntimeError
 
 
@@ -91,19 +91,19 @@ class Bootstrap(object):
 
 
     def install_ve(self):
-        executable = "%s/%s" % (self.bootstrap_dir, "ve")
-        self.download("https://raw.github.com/anjuke/ve/v1.0/ve",
-                      executable, "7f3a048a8f4a7a1c4e6c8d4cc67b5674")
+        executable = '%s/%s' % (self.bootstrap_dir, 've')
+        self.download('https://raw.github.com/anjuke/ve/v1.1/ve',
+                      executable, 'fd0f0601c6732ca9a5b3b62e691d68cb')
         self.mark_executable(executable)
 
 
     def ve(self, cmd):
-        os.system("%s/%s %s" % (self.bootstrap_dir, "ve", cmd))
+        os.system('%s/%s %s' % (self.bootstrap_dir, 've', cmd))
 
 
 _bootstrap = None
 
-__all_ = ["bootstrap", "ve"]
+__all_ = ['bootstrap', 've']
 
 def bootstrap(working_dir=None):
     global _bootstrap
